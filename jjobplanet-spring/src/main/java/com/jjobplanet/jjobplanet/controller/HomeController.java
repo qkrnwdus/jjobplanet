5e684fdac00de4aeb2111bdbce5a41502b4786a9
Cm4ng <dudrbs703@naver.com>
2022-02-28 22:23:17 +0900

    파일 일부 정리

--- jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
+++ jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
@@ -2,33 +2,26 @@ package com.jjobplanet.jjobplanet.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.jjobplanet.jjobplanet.MailService;
 import com.jjobplanet.jjobplanet.databasemanager.comjoinDB;
-import com.jjobplanet.jjobplanet.databasemanager.indvjoinDB;
 import com.jjobplanet.jjobplanet.databasemanager.loginDB;
 import com.jjobplanet.jjobplanet.databasemanager.noticeWriteDB;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HomeController {
@@ -166,9 +159,6 @@ public class HomeController {
 	
 	@RequestMapping(value = "/joinOk.do")
 	public String joinOk(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		//indvjoinDB injoin = new indvjoinDB();
-		//String result = injoin.doPost(request, response);
 	
 		return "joinOk";
 	}
