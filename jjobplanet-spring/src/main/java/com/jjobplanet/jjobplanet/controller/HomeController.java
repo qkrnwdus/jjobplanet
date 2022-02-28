64a5bff82ff5ff20a5ffb147ea622b727376ed70
kimbrong <97076313+kimbrong@users.noreply.github.com>
2022-02-28 19:51:52 +0900

    Notice View 부분 추가
    보이긴 하는데 코딩이 약간 쓰레기같음

--- jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
+++ jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.jjobplanet.jjobplanet.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
@@ -12,7 +13,11 @@ import javax.servlet.http.HttpSession;
 import com.jjobplanet.jjobplanet.databasemanager.comjoinDB;
 import com.jjobplanet.jjobplanet.databasemanager.indvjoinDB;
 import com.jjobplanet.jjobplanet.databasemanager.loginDB;
+import com.jjobplanet.jjobplanet.databasemanager.noticeViewDB;
 import com.jjobplanet.jjobplanet.databasemanager.noticeWriteDB;
+import com.jjobplanet.jjobplanet.domain.noticeDAO;
+import com.jjobplanet.jjobplanet.domain.noticeDTO;
+import com.mysql.cj.Session;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -49,11 +55,11 @@ public class HomeController {
 	public String search(HttpServletRequest request) {
 		
 		
-		String keyword = request.getParameter("key");
-		String category = request.getParameter("cat");
-		String region = request.getParameter("region");
-		String career = request.getParameter("career");
+		String q = request.getParameter("q");
+		Cookie cookie = new Cookie("search_history", q);
 		
+		cookie.setMaxAge(0);
+		cookie.setPath("/");
 
 		return "search";
 	}
@@ -124,10 +130,28 @@ public class HomeController {
 		return "recruitService";
 	}
 
-	@GetMapping("/notice")
-	public String notice()
+	@RequestMapping(value="/notice", method = RequestMethod.GET)
+	public ModelAndView notice(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-		return "notice";
+		noticeViewDB noticeviewDB = new noticeViewDB();
+		List<String> noticeviewList =  noticeviewDB.doViewNotice(request, response);
+		
+		// List<noticeDTO> noticeList = new ArrayList<noticeDTO>();
+		// noticeDTO ndao = new noticeDTO();
+		// ndao.setNoticeno("noticeno");
+		// ndao.setNoticetitle("noticetitle");
+		// ndao.setNoticedate("noticedate");
+		// ndao.setNoticenote("noticenote");
+		// noticeList.add(ndao);
+		
+		ModelAndView nvList = new ModelAndView();
+
+		nvList.addObject("noticeviewList",  noticeviewList);
+		nvList.setViewName("notice");
+
+		System.out.println("noticeviewList = " + noticeviewList);
+
+		return nvList;
 	}
 
 	@GetMapping("/noticeWrite.do")
--- jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/databasemanager/noticeViewDB.java
+++ jjobplanet-spring/src/main/java/com/jjobplanet/jjobplanet/databasemanager/noticeViewDB.java
@@ -6,18 +6,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 public class noticeViewDB 
 {
-	public void doViewNotice(HttpServletRequest request,  RedirectAttributes setAttribute) throws ServletException, IOException {
+	public List<String> doViewNotice(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	
 		String noticeno = request.getParameter("noticeno");
 		String noticetitle = request.getParameter("noticetitle");
@@ -27,30 +32,48 @@ public class noticeViewDB
 		String host   = "jdbc:mariadb://cm4ng.iptime.org:3307/jjobplanet?useUnicode=true&characterEncoding=utf-8&serverTimezone=UTC";
 		String userid = "mskim";
 		String passwd = "Sudwhd12!";
-		
+		List<String> noticeList = new ArrayList<>();
 		
 		try(Connection conn =  DriverManager.getConnection(host,userid,passwd);
 			Statement stmt = conn.createStatement()) 
 		{	
-			String sql = "select noticeno, noticetitle, noticedate, noticenote from notice";
+			String sql = "select * from notice";
 
 			System.out.println("SQL:" + sql);
 			ResultSet result = stmt.executeQuery(sql);
-		
-			noticeno = result.getString("noticeno");
-			noticetitle = result.getString("noticetitle");
-			noticedate = result.getString("noticedate");
-			noticenote = result.getString("noticenote");
 
-			HttpSession session = request.getSession();
-			session.setAttribute("noticeno",noticeno);
-			session.setAttribute("noticetitle",noticetitle);
-			session.setAttribute("noticedate",noticedate);
-			session.setAttribute("noticenote",noticenote);
-		
-		} catch (Exception e) {
+			while(result.next())
+			{
+				noticeno = result.getString("noticeno");
+				noticetitle = result.getString("noticetitle");
+				noticedate = result.getString("noticedate");
+				noticenote = result.getString("noticenote");
+
+				HttpSession session = request.getSession();
+				session.setAttribute("noticeno",noticeno);
+				session.setAttribute("noticetitle",noticetitle);
+				session.setAttribute("noticedate",noticedate);
+				session.setAttribute("noticenote",noticenote);
+
+				
+				// add() method
+				noticeList.add(noticeno);
+				noticeList.add(noticetitle);
+				noticeList.add(noticedate);
+				noticeList.add(noticenote);
+
+				//session.setAttribute("noticeList", noticeList);
+
+
+				
+			}
+
+			// System.out.println(noticeList);
+		} catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
+		return noticeList;
 			
 	}
 }
